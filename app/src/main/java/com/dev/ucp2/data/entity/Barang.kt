93e7f0a78eb9bfe096b74d38c7aaa9335ddf29fd
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 04:35:46 +0700

    memperbaiki atrribut-atribut pada  tabel barang

--- app/src/main/java/com/dev/ucp2/data/entity/Barang.kt
+++ app/src/main/java/com/dev/ucp2/data/entity/Barang.kt
@@ -3,17 +3,17 @@ package com.dev.ucp2.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "barang")
 data class Barang(
     @PrimaryKey
-    val id : String,
-    val nama : String,
-    val deskripsi : String,
-    val harga : String,
-    val stok : String,
-    val nama_suplier : String,
+    val idbarang: String,
+    val nama: String,
+    val deskripsi: String,
+    val harga: Int,
+    val stok: Int,
+    val nama_suplier: String
 )
+
 
 
 
