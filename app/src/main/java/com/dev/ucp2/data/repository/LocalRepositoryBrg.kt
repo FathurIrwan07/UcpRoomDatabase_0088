56cd0471ba8a086d866306e10ff557d7462873ee
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 05:30:42 +0700

    mengimplementasikan LocalRepositoryBrg dengan fungsi CRUD untuk Barang

--- app/src/main/java/com/dev/ucp2/data/repository/LocalRepositoryBrg.kt
+++ app/src/main/java/com/dev/ucp2/data/repository/LocalRepositoryBrg.kt
@@ -6,4 +6,20 @@ import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryBrg(
     private val barangDao: BarangDao
-)
+) : RepositoryBrg {
+    override suspend fun insertBarang(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+    override suspend fun deleteBarang(barang: Barang) {
+        barangDao.deleteBarang(barang)
+    }
+    override suspend fun updateBarang(barang: Barang) {
+        barangDao.updateBarang(barang)
+    }
+    override fun getAllBarang(): Flow<List<Barang>> =
+        barangDao.getAllBarang()
+
+    override fun getBarang(id: String): Flow<Barang> =
+        barangDao.getBarang(id)
+
+}
