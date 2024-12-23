c04cf0a19dd2ec22d8693c8e62633f51a36d7b8e
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 05:01:30 +0700

    menambahkan interface RepositoryBrg untuk operasi Barang

--- app/src/main/java/com/dev/ucp2/data/repository/RepositoryBrg.kt
+++ app/src/main/java/com/dev/ucp2/data/repository/RepositoryBrg.kt
@@ -1,4 +1,12 @@
 package com.dev.ucp2.data.repository
 
+import com.dev.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryBrg {
+    suspend fun insertBarang(barang: Barang)
+    fun getAllBarang(): Flow<List<Barang>>
+    fun getBarang(id: String): Flow<Barang>
+    suspend fun deleteBarang(barang: Barang)
+    suspend fun updateBarang(barang: Barang)
 }
