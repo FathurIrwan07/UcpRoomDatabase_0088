eddc7589c01c2a3dd397d4a0827c5bf0f71f8f41
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 05:39:16 +0700

    menambahkan kelas LocalRepositorySpl dengan dependensi SuplierDao

--- app/src/main/java/com/dev/ucp2/data/repository/LocalRepositorySpl.kt
+++ app/src/main/java/com/dev/ucp2/data/repository/LocalRepositorySpl.kt
@@ -1,4 +1,10 @@
 package com.dev.ucp2.data.repository
 
-class LocalRepositorySpl {
-}
+
+import com.dev.ucp2.data.dao.SuplierDao
+import com.dev.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySpl(
+    private val suplierDao: SuplierDao
+)
