83679387e3f603c541f73ff41572b8e8ebe75416
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 04:43:57 +0700

    memperbaiki atrribut-atribut pada  tabel suplier

--- app/src/main/java/com/dev/ucp2/data/entity/Suplier.kt
+++ app/src/main/java/com/dev/ucp2/data/entity/Suplier.kt
@@ -4,12 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
+
 @Entity(tableName = "suplier")
 data class Suplier(
     @PrimaryKey
-    val id : String,
-    val nama_suplier : String,
-    val kontak : String,
-    val alamat : String,
-
+    val idsuplier: String,
+    val nama_suplier: String,
+    val kontak: String,
+    val alamat: String,
 )
