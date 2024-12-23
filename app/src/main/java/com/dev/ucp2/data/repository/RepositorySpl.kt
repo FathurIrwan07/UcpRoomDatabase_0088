3c67cc2056497c81ae1c8bd9c2c7a34d56ba550c
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 05:07:38 +0700

    menambahkan interface RepositorySpl untuk operasi Suplier

--- app/src/main/java/com/dev/ucp2/data/repository/RepositorySpl.kt
+++ app/src/main/java/com/dev/ucp2/data/repository/RepositorySpl.kt
@@ -1,4 +1,12 @@
 package com.dev.ucp2.data.repository
 
+
+import com.dev.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
 interface RepositorySpl {
+    suspend fun insertSuplier(suplier: Suplier)
+    fun getAllSuplier(): Flow<List<Suplier>>
+    fun getSuplier(id: String): Flow<Suplier>
 }
+
