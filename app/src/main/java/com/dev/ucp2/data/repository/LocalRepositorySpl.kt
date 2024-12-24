0451d30b283815285833d96e0084f839d23e2242
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 07:41:21 +0700

    mengimplementasikan RepositorySpl dengan fungsi create dan read untuk Suplier

--- app/src/main/java/com/dev/ucp2/data/repository/LocalRepositorySpl.kt
+++ app/src/main/java/com/dev/ucp2/data/repository/LocalRepositorySpl.kt
@@ -7,4 +7,13 @@ import kotlinx.coroutines.flow.Flow
 
 class LocalRepositorySpl(
     private val suplierDao: SuplierDao
-)
+) : RepositorySpl {
+    override suspend fun insertSuplier(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+    override fun getAllSuplier(): Flow<List<Suplier>> =
+        suplierDao.getAllSuplier()
+
+    override fun getSuplier(id: String): Flow<Suplier> =
+        suplierDao.getSuplier(id)
+}
