89ead46411536b8b37155add024a89cd3b48bf50
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 08:02:39 +0700

    menambahkan fungsi TopAppBar dengan opsi tombol kembali dan judul dan ganti warna background

--- app/src/main/java/com/dev/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/dev/ucp2/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,37 @@
 package com.dev.ucp2.ui.customwidget
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dev.ucp2.R
+
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier,
+) {
+    val backgroundColor = Color(0xFF6200EE)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(backgroundColor)
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+
+    }
+}
