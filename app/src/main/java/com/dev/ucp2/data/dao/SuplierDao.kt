b49d8bbf096fbb81e45168c28785618a00828a00
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 09:17:55 +0700

    perbaiki dari id menjadi idsuplier pada interface SuplierDao agar sesuai dengan tabel suplier

--- app/src/main/java/com/dev/ucp2/data/dao/SuplierDao.kt
+++ app/src/main/java/com/dev/ucp2/data/dao/SuplierDao.kt
@@ -1,11 +1,13 @@
 package com.dev.ucp2.data.dao
 
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.dev.ucp2.data.entity.Suplier
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SuplierDao {
     @Insert
     suspend fun insertSuplier(
@@ -15,7 +17,7 @@ interface SuplierDao {
     @Query("SELECT * FROM suplier ORDER BY nama_suplier ASC")
     fun getAllSuplier(): Flow<List<Suplier>>
 
-    @Query("SELECT * FROM suplier WHERE id = :id")
+    @Query("SELECT * FROM suplier WHERE idsuplier = :id")
     fun getSuplier(id: String): Flow<Suplier>
 
 }
