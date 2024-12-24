03b2af7ff10be20bc4ea1cd04d87fb6b1020fe12
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-24 11:04:39 +0700

    menambahkan implementasi AlamatNavigasi dengan berbagai destinasi navigasi

--- app/src/main/java/com/dev/ucp2/ui/navigasi/AlamatNavigasi.kt
+++ app/src/main/java/com/dev/ucp2/ui/navigasi/AlamatNavigasi.kt
@@ -1,4 +1,50 @@
 package com.dev.ucp2.ui.navigasi
 
+
 interface AlamatNavigasi {
+    val route: String
+}
+
+
+object DestinasiSplash : AlamatNavigasi {
+    override val route = "splash"
+}
+
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiHomeSpl: AlamatNavigasi {
+    override val route = "homesuplier"
+}
+
+object DestinasiHomeBrg: AlamatNavigasi {
+    override val route = "homebarang"
+}
+
+object DestinasiInsertSpl: AlamatNavigasi {
+    override val route = "suplier"
+}
+
+object DestinasiInsertBrg: AlamatNavigasi {
+    override val route = "barang"
+}
+
+object DestinasiDetailSpl: AlamatNavigasi {
+    override val route = "detail"
+    const val idSuplier = "idSuplier"
+    val routeWithArg = "$route/{$idSuplier}"
+}
+
+object DestinasiDetailBrg: AlamatNavigasi {
+    override val route = "detail"
+    const val idBarang = "idBarang"
+    val routeWithArg = "$route/{$idBarang}"
+}
+
+object DestinasiUpdate: AlamatNavigasi {
+    override val route = "update"
+    const val idBarang = "idBarang"
+    val routeWithArg = "$route/{$idBarang}"
 }
